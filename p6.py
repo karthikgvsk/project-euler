84f181871035a6a8452994d80ec4c04ac45b5fde
karthikgvsk <karthikgvsk@gmail.com>
2013-01-05 18:11:44 +0530

    second commit

--- p6.py
+++ p6.py
@@ -1,2 +1,26 @@
-sdfsad
 
+def palindrome(n):
+	s1 = str(n)
+	#if s1 is equal to its reverse, return True
+	if s1 == s1[::-1]:
+		return True
+	else:
+		return False
+
+start = 999
+end = 100
+max = 0
+m = start
+while m >= end:
+	n = start
+	while n >= m:
+		prod = m * n
+		if max >= prod:
+			break
+		if palindrome(m * n):
+			max = m * n
+		
+		n = n - 1
+	m = m - 1
+
+print max
